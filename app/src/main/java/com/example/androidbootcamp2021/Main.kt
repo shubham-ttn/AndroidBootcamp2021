f0dfb158476c68534fd435d7ccc0f44519d88175
Shubham Pandey <shubham.pandey@tothenew.com>
2021-02-18 14:29:02 +0530

    Added Q3

--- app/src/main/java/com/example/androidbootcamp2021/Main.kt
+++ app/src/main/java/com/example/androidbootcamp2021/Main.kt
@@ -27,4 +27,15 @@ fun main() {
     ob2.multiplyInt(4, 4)
     ob2.concatString("Hello ", "World")
     ob2.concatThreeStrings("This ", "is ", "Kotlin ")
+
+    // Q3 Create 3 sub class of bank SBI,BOI,ICICI all 4 should have method called getDetails which provide
+    // there specific details like rateofinterest etc,print details of every bank.
+    println("\nQ3")
+    val ob3 = SBI()
+    ob3.getBankDetails()
+    val ob4 = BOI()
+    ob4.getBankDetails()
+    val ob5 = ICICI()
+    ob5.getBankDetails()
+
 }
