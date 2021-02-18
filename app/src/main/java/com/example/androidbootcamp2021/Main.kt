6c50decafce87125606356bd2a8cbf1359257fd5
Shubham Pandey <shubham.pandey@tothenew.com>
2021-02-18 15:27:55 +0530

    Added qsn 5

--- app/src/main/java/com/example/androidbootcamp2021/Main.kt
+++ app/src/main/java/com/example/androidbootcamp2021/Main.kt
@@ -48,4 +48,13 @@ fun main() {
     ob6.getBookDetails()
     ob7.getBookDetails()
 
+    // Q5 Write a function which take marks as an argument and return the the grade as follows:
+    //marks between 50 to 60 , return “Good”
+    //marks between 60 to 70, return “Very Good”
+    //marks between 70 to  80, return “Excellent”
+    //marks between  80 to 100, return “Rockstar”
+    println("\nQ5")
+    val ob8 = Q5()
+    println("Your grade: ${ob8.getGradeDetails(80)}")
+
 }
