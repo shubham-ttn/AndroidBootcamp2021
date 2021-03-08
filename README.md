e5f9a4fa8430d4e85d6196d86062c8c7c0177806
Shubham Pandey <shubham.pandey@tothenew.com>
2021-03-08 21:29:25 +0530

    Added Qsn 2 with Output

--- README.md
+++ README.md
@@ -8,3 +8,7 @@
 * Make an application with one edit text, Two buttons(Submit & retrieve) and one textview .On clicking the Submit button data provided in the edit text should be stored in shared preferences and on clicking the retrieve button data should be retrieved from the shared preferences and get set on the text view provided.
 
 <img src="output2.png" width="300" height="600" />
+
+* Make an app that has one edit text and two buttons called Write and read. App should save text provided in the edit text as a txt file on internal/external storage on clicking on Write button. Upon clicking on read button edit text should get cleared and the text from the saved file should be populated.
+
+<img src="output3.png" width="300" height="600" />
--- app/src/main/java/com/example/androidbootcamp2021/FileHandlingActivity.kt
+++ app/src/main/java/com/example/androidbootcamp2021/FileHandlingActivity.kt
@@ -1,11 +1,88 @@
 package com.example.androidbootcamp2021
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_file_handling.*
+import java.io.FileInputStream
 
 class FileHandlingActivity : AppCompatActivity() {
+
+    companion object {
+        const val FILE_NAME = "myFile"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_file_handling)
+
+        fileHandlingWriteBtn.setOnClickListener {
+            writeToFile()
+        }
+
+        fileHandlingReadBtn.setOnClickListener {
+            readFromFile()
+        }
+
+    }
+
+    private fun readFromFile() {
+        try {
+            val fIn: FileInputStream = openFileInput(FILE_NAME)
+
+            var c: Int
+            var stringTemp = ""
+
+            while (fIn.read().also { c = it } != -1) {
+                stringTemp += c.toChar().toString()
+            }
+
+            if (stringTemp.isEmpty()) {
+                Toast.makeText(
+                    this,
+                    "Enter text and then click Submit button first",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                fileHandlingEditText.setText(stringTemp)
+            }
+            fIn.close()
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun writeToFile() {
+        try {
+            // Open file and specify the mode
+            val fOut = openFileOutput(FILE_NAME, Context.MODE_PRIVATE)
+
+            // Get text
+            val textToWrite: String? = fileHandlingEditText.text.toString()
+
+            if (!textToWrite.isNullOrEmpty()) {
+                fileHandlingEditText.text!!.clear()
+
+                // Write to file in ByteArray
+                fOut.write(textToWrite.toByteArray())
+
+                // Close the connection
+                fOut.close()
+
+                Toast.makeText(this, "Now click on Read button to get the text", Toast.LENGTH_LONG)
+                    .show()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Enter text and then click Submit button first",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
