1ae6d09f17514aac331d0f998d8881c89a0df07c
Shubham Pandey <shubham.pandey@tothenew.com>
2021-03-29 18:52:22 +0530

    Added Fibonacci demo using Coroutines

--- app/src/main/java/com/example/androidbootcamp2021/Main.kt
+++ app/src/main/java/com/example/androidbootcamp2021/Main.kt
@@ -1,5 +1,24 @@
 package com.example.androidbootcamp2021
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
 fun main() {
-    println("Hello World!")
+    GlobalScope.launch { // launch a new coroutine in background and continue
+        printFibonacciSeries(10) // print after delay
+    }
+    println("Fibonacci series of 5 is:") // main thread continues while coroutine is delayed
+    Thread.sleep(10000L) // block main thread for 2 seconds to keep JVM alive
+}
+
+suspend fun printFibonacciSeries(endRange: Int) {
+        var t1 = 0; var t2 = 1; var nextTerm = 0;
+        for (i in 1..endRange) {
+            print(" $t1")
+            nextTerm = t1 + t2;
+            t1 = t2;
+            t2 = nextTerm;
+            delay(300)
+        }
 }
