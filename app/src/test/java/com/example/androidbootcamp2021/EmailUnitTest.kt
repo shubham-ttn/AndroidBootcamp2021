0988014baec8e04a031b3325b7c72919fca83e07
Shubham Pandey <shubham.pandey@tothenew.com>
2021-04-01 12:49:12 +0530

    Completed JUnit testing with output

--- app/src/test/java/com/example/androidbootcamp2021/EmailUnitTest.kt
+++ app/src/test/java/com/example/androidbootcamp2021/EmailUnitTest.kt
@@ -5,8 +5,36 @@ import org.junit.Test
 
 class EmailUnitTest {
     @Test
+    fun testEmailShouldNotContainSpaces_isCorrect() {
+        val emailAddress = "shubh am@gmail.com"
+        val emailIsCorrect = signUpWithEmailOnly(emailAddress)
+        Assert.assertTrue(emailIsCorrect)
+    }
+
+    @Test
+    fun testEmailShouldContainCorrectDomain_isCorrect() {
+        val emailAddress = "shubham@gmail.com"
+        val emailIsCorrect = signUpWithEmailOnly(emailAddress)
+        Assert.assertTrue(emailIsCorrect)
+    }
+
+    @Test
+    fun testEmailShouldNotBeBlank_isCorrect() {
+        val emailAddress = ""
+        val emailIsCorrect = signUpWithEmailOnly(emailAddress)
+        Assert.assertTrue(emailIsCorrect)
+    }
+
+    @Test
     fun testEmailFormat_isCorrect() {
         val emailAddress = "shubham@gmail.com"
+        val emailIsCorrect = signUpWithEmailOnly(emailAddress)
+        Assert.assertTrue(emailIsCorrect)
+    }
+
+    @Test
+    fun testEmailShouldNotStartWithUnderScore_isCorrect() {
+        val emailAddress = "helloshubham@gmail.com"
         val emailIsCorrect = signUpWithEmailOnly(emailAddress)
         Assert.assertTrue(emailIsCorrect)
     }
